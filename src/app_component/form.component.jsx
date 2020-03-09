15ce9e0a398e02db28f35fb21d5ff5fd10142339
Bojan <markovski.bojan@hotmail.com>
2020-03-09 22:18:17 +0100

    Weather forecast hard coded

---
+++ src/app_component/form.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Form = props => {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3 col-md-offset-2">
+                    <input type="text" className="form-control" name="city" autoComplete="off"/>
+                </div>
+                <div className="col-md-3">
+                    <input type="text" className="form-control" name="country" autoComplete="off"/>
+                </div>
+                <div className="col-md-3 mt-md-0 text-md-left">
+                    <button className="btn btn-warning">Get wather</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Form;
---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,28 @@
+import  React from 'react';
+
+const Weather = (props) => {
+    return(
+        <div className="container">
+            <div className="cards">
+                <h1>Weather App</h1>
+                <h2>{props.city}, {props.country}</h2>
+                <h5 className="py-4"></h5>
+                <i className={`wi ${props.weatherIcon} display-1`}></i>
+                <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+                {minmaxTemp(props.temp_min, props.temp_max)}
+                <h4 className="py-3">{props.description}</h4>
+            </div>
+        </div>
+    );
+};
+
+function minmaxTemp(min, max){
+    return(
+        <h3>
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+        </h3>
+    );
+};
+
+export default Weather;
